9873087642ee636f0527c5fd4a0a4dc589a3805c
Sergey Mironov <ka6ah505@gmail.com>
2018-06-12 15:38:53 +0300

    Добавлено склонение числительных

--- task3.js
+++ task3.js
@@ -4,7 +4,17 @@
 
 var summa = parseInt(process.argv[2]) + parseInt(process.argv[3]);
 
+function declination(number, titles) {  
+	cases = [2, 0, 1, 1, 1, 2];  
+	return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
+}
+
 var hours = Math.floor(summa / 3600); //определение часов
 var minutes = Math.floor((summa%3600) / 60); // определение минут
-var seconds = summa % 60;
-console.log(hours+" часов "+minutes+" минут "+seconds+" секунд");
+var seconds = summa % 60; //определение секунд
+
+var title1 = declination(hours, [' час ', ' часа ', ' часов ']);
+var title2 = declination(minutes, [' минута ', ' минуты ',' минут ']);
+var title3 = declination(seconds, [' секунда',' секунды',' секунд']);
+
+console.log(hours+title1+minutes+title2+seconds+title3);
