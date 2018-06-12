a9374d76668c685b577fd053d608b8d1f823fa42
Sergey Mironov <ka6ah505@gmail.com>
2018-06-12 08:57:03 +0300

    Проверка для перестановок в одну сторону

--- task2.js
+++ task2.js
@@ -1,3 +1,19 @@
 /* Task 2 */
 
 /* Put your code here */
+
+var a = process.argv[2].split("");
+var b = process.argv[3].split("");
+
+var test = function (mass1, mass2) {
+	var l = mass1.length;
+	for (var i = 1; i < l; i++) {
+		mass1 = mass1.splice(1).concat(mass1);
+		if (mass1.join()==mass2.join()) {
+			return i;
+		}
+	};
+	return -1;	
+}
+
+console.log(test(a, b));
