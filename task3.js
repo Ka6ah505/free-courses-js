2256956c62fe657565a75e2f511ba9183436bc87
Sergey Mironov <ka6ah505@gmail.com>
2018-06-12 15:18:43 +0300

    Определена логика вычисления единиц времени

--- task3.js
+++ task3.js
@@ -1,3 +1,10 @@
 /* Task 3 */
 
 /* Put your code here */
+
+var summa = parseInt(process.argv[2]) + parseInt(process.argv[3]);
+
+var hours = Math.floor(summa / 3600); //определение часов
+var minutes = Math.floor((summa%3600) / 60); // определение минут
+var seconds = summa % 60;
+console.log(hours+" часов "+minutes+" минут "+seconds+" секунд");
