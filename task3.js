8a4207a582d48c3aa010c5b808807daf6e014c7f
Sergey Mironov <ka6ah505@gmail.com>
2018-06-14 09:14:57 +0300

    Изменение вывода времени

--- task3.js
+++ task3.js
@@ -13,8 +13,17 @@ var hours = Math.floor(summa / 3600); //определение часов
 var minutes = Math.floor((summa%3600) / 60); // определение минут
 var seconds = summa % 60; //определение секунд
 
-var title1 = declination(hours, [' час ', ' часа ', ' часов ']);
-var title2 = declination(minutes, [' минута ', ' минуты ',' минут ']);
-var title3 = declination(seconds, [' секунда',' секунды',' секунд']);
+var title1 = "";
+var title2 = "";
+var title3 = "";
+if (hours > 0) {
+	title1 = declination(hours, [' час ', ' часа ', ' часов ']);
+}
+if (minutes > 0) {
+	title2 = declination(minutes, [' минута ', ' минуты ',' минут ']);
+}
+if (seconds > 0) {
+	title3 = declination(seconds, [' секунда',' секунды',' секунд']);
+}
 
 console.log(hours+title1+minutes+title2+seconds+title3);
